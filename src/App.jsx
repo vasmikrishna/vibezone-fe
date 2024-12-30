9fb6b421837111e17b83f80e8792ffc2c5f0f55c
vkreddycom <vkreddy.com@gmail.com>
2024-12-30 22:21:33 +0530

    modified the ui

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ export default function App() {
   useEffect(() => {
     // 1. Connect to signaling server
     // IMPORTANT: Use "ws://localhost:3001", not "http://"
-    // wsRef.current = new WebSocket('http://localhost:3001/');
-    wsRef.current = new WebSocket('https://vibezone.in/');
+    wsRef.current = new WebSocket('http://localhost:3001/');
+    // wsRef.current = new WebSocket('https://vibezone.in/');
 
 
     wsRef.current.onopen = () => {
